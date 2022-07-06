54e89b3a32d1ac18a140de5eddbd0d151e3699ab
Arlisson Costa <Arlisson.fs13@gmail.com>
2022-07-05 23:05:50 -0300

    Adicionando funcoes login

--- Client/src/pages/Alunos/Alunos.jsx
+++ Client/src/pages/Alunos/Alunos.jsx
@@ -1,21 +1,56 @@
 import styles from "./Alunos.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import { Trash, Pen, SignOut, User } from "phosphor-react";
+import { useEffect, useState } from "react";
+import api from "../../services/api";
 
 const Alunos = () => {
+  const [name, setName] = useState("");
+  const [alunos, setAlunos] = useState([]);
+
+  const Navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+  const email = localStorage.getItem("email");
+  const token = localStorage.getItem("token");
+
+  const authorization = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const getAlunos = () => {
+    api.get("api/alunos", authorization).then((response) => {
+      setAlunos(response.data);
+      setLoading(false);
+    }, token);
+  };
+
+  useEffect(() => {
+    getAlunos();
+  }, []);
+
+  const logout = () => {
+    localStorage.clear();
+    localStorage.setItem("token", "");
+    authorization.headers = "";
+    Navigate("/");
+  };
+
   return (
     <div className="container d-flex justify-content-center flex-column">
       <header>
         <div className="d-flex justify-content-around mt-5">
           <span>
             <User size={19} className="m-2" />
-            Bem-vindo, <strong>Arlisson</strong>
+            Bem-vindo, <strong>{email}</strong>
           </span>
           <Link className="btn btn-success" to="/aluno/novo/0">
             <span>Novo Aluno</span>
           </Link>
-          <button type="button" className="btn">
+          <button type="button" onClick={logout} className="btn">
             <SignOut size={22} />
           </button>
         </div>
@@ -27,53 +62,34 @@ const Alunos = () => {
         </button>
       </form>
       <h1>Relação de Alunos</h1>
+      {loading && <p>Aguarde...</p>}
       <ul className="row justify-content-between mt-3 w-100">
-        <li className={`card ${styles.card_width}`}>
-          <div className="d-flex justify-content-between p-4">
-            <div className="d-flex flex-column gap-3">
-              <b>
-                Nome: <b>Paulo</b>
-              </b>
-              <b>
-                E-mail: <b>paulo@email.com</b>
-              </b>
-              <b>
-                Idade: <b>22</b>
-              </b>
-            </div>
-            <div className="d-flex flex-column gap-2">
-              <button type="button" className="btn">
-                <Pen size={24} />
-              </button>
-              <button type="button" className="btn">
-                <Trash size={24} />
-              </button>
-            </div>
-          </div>
-        </li>
-        <li className={`card ${styles.card_width}`}>
-          <div className="d-flex justify-content-between p-4">
-            <div className="d-flex flex-column gap-3">
-              <b>
-                Nome: <b>Paulo</b>
-              </b>
-              <b>
-                E-mail: <b>paulo@email.com</b>
-              </b>
-              <b>
-                Idade: <b>22</b>
-              </b>
-            </div>
-            <div className="d-flex flex-column gap-2">
-              <button type="button" className="btn">
-                <Pen size={24} />
-              </button>
-              <button type="button" className="btn">
-                <Trash size={24} />
-              </button>
-            </div>
-          </div>
-        </li>
+        {alunos &&
+          alunos.map((aluno) => (
+            <li key={aluno.id} className={`card ${styles.card_width}`}>
+              <div className="d-flex justify-content-between p-4">
+                <div className="d-flex flex-column gap-3">
+                  <b>
+                    Nome: <b>{aluno.nome}</b>
+                  </b>
+                  <b>
+                    E-mail: <b>{aluno.email}</b>
+                  </b>
+                  <b>
+                    Idade: <b>{aluno.idade}</b>
+                  </b>
+                </div>
+                <div className="d-flex flex-column gap-2">
+                  <button type="button" className="btn">
+                    <Pen size={24} />
+                  </button>
+                  <button type="button" className="btn">
+                    <Trash size={24} />
+                  </button>
+                </div>
+              </div>
+            </li>
+          ))}
       </ul>
     </div>
   );
--- Client/src/pages/Login/Login.jsx
+++ Client/src/pages/Login/Login.jsx
@@ -1,26 +1,69 @@
 import styles from "./Login.module.css";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { UserCircle, LockKey } from "phosphor-react";
+import api from "../../services/api";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPaswword] = useState("");
+
+  const Navigate = useNavigate();
+
+  const login = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      email,
+      password,
+    };
+
+    try {
+      const response = await api.post("api/account/loginuser", data);
+      localStorage.setItem("email", email);
+      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("expiration", response.data.expiration);
+      console.log(response);
+      Navigate("/alunos");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="container d-flex justify-content-center align-items-center vh-100">
       <section>
-        <form className={`d-flex flex-column w-100 ${styles.form}`}>
+        <form
+          onSubmit={login}
+          className={`d-flex flex-column w-100 ${styles.form}`}
+        >
           <h1 className="mb-3">Cadastro de Alunos</h1>
           <div className="position-relative">
             <UserCircle
               className={`position-absolute ${styles.icon_login}`}
               size={24}
             />
-            <input className="w-100" type="email" placeholder="E-mail" />
+            <input
+              className="w-100"
+              type="email"
+              placeholder="E-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="position-relative">
             <LockKey
               className={`position-absolute ${styles.icon_login}`}
               size={24}
             />
-            <input className="w-100" type="password" placeholder="Senha" />
+            <input
+              className="w-100"
+              type="password"
+              placeholder="Senha"
+              value={password}
+              onChange={(e) => setPaswword(e.target.value)}
+            />
           </div>
 
           <button className="btn btn-success" type="submit">
